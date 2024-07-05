69c9a9841f80a78be1bb0eef2a8294ea4014fbe5
GustavoPacheco <gugapachecops4@gmail.com>
2024-07-04 22:04:53 -0300

    sim

---
+++ condicional/src/SistemaDeMedida.java
@@ -0,0 +1,21 @@
+public class SistemaDeMedida {
+    public static void main(String[] args) {
+        String sigla = "m";
+
+        switch (sigla) {
+            case "G":
+                System.out.println("grande");
+            break;
+            case "M" :
+                System.out.println("Médio");
+            break;
+            case "P" :
+            System.out.println("Pequeno");
+            break;
+
+            default: 
+            System.out.println("Valor indefinido");
+            break;
+        }
+    }
+}
