2529a795c826742e2cc86a8a507cc408c2846b7b
GustavoPacheco <gugapachecops4@gmail.com>
2024-07-09 23:54:05 -0300

    sim

---
+++ basico/src/repetição/carneirinhos.java
@@ -0,0 +1,11 @@
+package src.repetição;
+
+public class carneirinhos {
+    public static void main(String[] args) {
+        for(int carneirinhos = 1; carneirinhos <= 20 ; carneirinhos ++){
+            System.out.println("contando carneirinhos " + carneirinhos);
+        } 
+        System.out.println("Joazinho dormiu...");
+        
+    }
+}
